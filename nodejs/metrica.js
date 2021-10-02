1fc7540c48a79ca478262006131dd1da7622bd96
mezin <z2941@ya.ru>
2021-10-02 22:19:04 +0300

    test

--- nodejs/metrica.js
+++ nodejs/metrica.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
-const mysql = require("mysql2");
-let config = require("./config.js");
+var pool = require("./config.js");
 
 // (async () => {
 //   const browser = await puppeteer.launch();
@@ -36,10 +35,11 @@ let getMetrica = async () => {
     deviceScaleFactor: 1,
   });
   // let domains = ['buro39.ru', 'shm.ru', 'mosmuseum.ru'];
-  let domains = ['ostrovok.ru'];
+  // let domains = ['ostrovok.ru'];
+  // let domains = ['rosphoto.com'];
+  let domains = ['the-village.ru'];
   let screen_path = '../storage/web/screen/';
   let chart_path = '../storage/web/chart/';
-  // let data = [];
 
   for (var domain of domains){ // Проходимся в цикле по каждому товару
     // try {
@@ -47,9 +47,9 @@ let getMetrica = async () => {
     // } catch(error) {
     //   console.log(error);
     // }
-    await page.goto('http://'+domain);
+    try {
+      await page.goto('http://'+domain);
     await page.screenshot({path: screen_path+domain+'.jpeg', type: 'jpeg', quality: 60});
- 
     await page.goto('https://pro.metrica.guru/id?domains='+domain)
     await page.waitForSelector('.bar_value', {visible: true,});
     await page.waitForSelector('.wrapper_line_chart');          // дожидаемся загрузки селектора
@@ -72,23 +72,15 @@ let getMetrica = async () => {
     const data = result;
     const sql = "INSERT INTO domain(domain, traffic, organic, direct, screen, chart) VALUES(?, ?, ?, ?, ?, ?)";
     
-    connection = mysql.createConnection({
-      host: config.host,
-      user: config.user,
-      database: config.database,
-      password: config.pass
+    pool.query(sql, data, function (error, results) {
+      if (error) throw error;
+      console.log('Данные добавлены');
     });
-
-    connection.query(sql, data, function(err) {
-        if(err) console.log(err);
-        else console.log("Данные добавлены");
-    });
-
-  // data.push(result);
+    } catch(error) {
+      console.log(error);
+    }
   }
-  connection.end();
   browser.close();
-  // return data;
 }
 
 getMetrica().then((value) => {
