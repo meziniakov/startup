830412dacbc56a0c88616f716f996bada6aab685
mezin <z2941@ya.ru>
2021-10-10 17:28:32 +0300

    add

---
+++ backend/models/search/ProjectSearch.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace backend\models\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Project;
+
+/**
+ * ProjectSearch represents the model behind the search form about `common\models\Project`.
+ */
+class ProjectSearch extends Project
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'author_id', 'created_at', 'updated_at'], 'integer'],
+            [['name', 'keywords'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params, $currentUserId)
+    {
+        $query = Project::find()->where(['author_id' => $currentUserId]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'keywords', $this->keywords]);
+
+        return $dataProvider;
+    }
+}
