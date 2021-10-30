6709e9f8e74b278695dd80bb8ea3ae8eb2a9dcb2
mezin <z2941@ya.ru>
2021-10-30 10:43:27 +0300

    fix

--- nodejs/be1.js
+++ nodejs/be1.js
@@ -57,77 +57,77 @@ let getBe1 = async () => {
   // for (var domain of domains){ // Проходимся в цикле по каждому товару
     try {
     // await page.goto('http://'+domain);
-    // await page.screenshot({path: screen_path+domain+'.jpeg', type: 'jpeg', quality: 60});
     // await page.goto('https://pro.metrica.guru/id?domains='+domain)
     await page.goto('https://be1.ru/stat/'+domain, {waitUntil: 'networkidle0'});
     await page.solveRecaptchas();
     // await Promise.all([
-    //   page.waitForNavigation(),
-    //   page.click(`#recaptcha-demo-submit`)
-    // ])
-    // const element = await page.$('.wrapper_line_chart');        // объявляем переменную с ElementHandle
-    // await element.screenshot({path: chart_path+domain+'_chart.jpeg', type: 'jpeg', quality: 80});
+      //   page.waitForNavigation(),
+      //   page.click(`#recaptcha-demo-submit`)
+      // ])
+      // const element = await page.$('.wrapper_line_chart');        // объявляем переменную с ElementHandle
+      // await element.screenshot({path: chart_path+domain+'_chart.jpeg', type: 'jpeg', quality: 80});
+      await page.screenshot({path: screen_path+domain+'.jpeg', type: 'jpeg', quality: 60});
 
-    const be1 = await page.evaluate((domain) => {
-      let traffic = document.querySelector('#similar_attendance text:nth-child(2)');
-      // traffic = traffic ? Number.parseInt(traffic.textContent.replace(/\s/g, '')) : '';
-      traffic = Number.parseInt(traffic.textContent.replace(/\s/g, ''));
+    // const be1 = await page.evaluate((domain) => {
+    //   let traffic = document.querySelector('#similar_attendance text:nth-child(2)');
+    //   // traffic = traffic ? Number.parseInt(traffic.textContent.replace(/\s/g, '')) : '';
+    //   traffic = Number.parseInt(traffic.textContent.replace(/\s/g, ''));
       
-      let direct = document.querySelector('#similar_source > div > div:nth-child(1) > div > svg > g:nth-child(4) > text');
-      direct = direct ? Number.parseInt(direct.textContent.replace(/\s/g, '%'))/100 : '';
+    //   let direct = document.querySelector('#similar_source > div > div:nth-child(1) > div > svg > g:nth-child(4) > text');
+    //   direct = direct ? Number.parseInt(direct.textContent.replace(/\s/g, '%'))/100 : '';
       
-      let organic = document.querySelector('#similar_source div div:nth-child(1) div svg g:nth-child(8) text');
-      organic = organic ? Number.parseInt(organic.textContent.replace(/\s/g, '%'))/100 : '';
+    //   let organic = document.querySelector('#similar_source div div:nth-child(1) div svg g:nth-child(8) text');
+    //   organic = organic ? Number.parseInt(organic.textContent.replace(/\s/g, '%'))/100 : '';
 
-      let traffic_season = (traffic !== '') ? (traffic*0.9).toFixed(0) : '';
-      let project_stage = 10;
-      let profit_await = (traffic_season === '' || organic === '') ? '' : (traffic_season*organic*0.3+(100-organic)*0.3/2).toFixed(0);
-      let evaluate_min = profit_await === '' ? '' : (project_stage*profit_await*0.5).toFixed(0);
-      let evaluate_middle = profit_await === '' ? '' : (project_stage*profit_await*0.75).toFixed(0);
-      let evaluate_max = profit_await === '' ? '' : (project_stage*profit_await).toFixed(0);
+    //   let traffic_season = (traffic !== '') ? (traffic*0.9).toFixed(0) : '';
+    //   let project_stage = 10;
+    //   let profit_await = (traffic_season === '' || organic === '') ? '' : (traffic_season*organic*0.3+(100-organic)*0.3/2).toFixed(0);
+    //   let evaluate_min = profit_await === '' ? '' : (project_stage*profit_await*0.5).toFixed(0);
+    //   let evaluate_middle = profit_await === '' ? '' : (project_stage*profit_await*0.75).toFixed(0);
+    //   let evaluate_max = profit_await === '' ? '' : (project_stage*profit_await).toFixed(0);
 
-      let domain_age = document.querySelector('#set_vozrast');
-      domain_age = domain_age ? domain_age.textContent : '';
+    //   let domain_age = document.querySelector('#set_vozrast');
+    //   domain_age = domain_age ? domain_age.textContent : '';
 
-      let IKS = document.querySelector('#set_iks');
-      IKS = IKS ? Number.parseInt(IKS.innerText.replace(/\s/g, '')) : '';
+    //   let IKS = document.querySelector('#set_iks');
+    //   IKS = IKS ? Number.parseInt(IKS.innerText.replace(/\s/g, '')) : '';
 
-      let index_Y = document.querySelector('#set_pages_in_yandex a');
-      index_Y = index_Y ? index_Y.textContent : '';
+    //   let index_Y = document.querySelector('#set_pages_in_yandex a');
+    //   index_Y = index_Y ? index_Y.textContent : '';
 
-      let index_G = document.querySelector('#set_pages_in_google a');
-      index_G = index_G ? index_G.textContent : '';
+    //   let index_G = document.querySelector('#set_pages_in_google a');
+    //   index_G = index_G ? index_G.textContent : '';
 
-      let megaindexTrustRank = document.querySelector('#set_trust_rank');
-      megaindexTrustRank = megaindexTrustRank ? megaindexTrustRank.innerText : '';
+    //   let megaindexTrustRank = document.querySelector('#set_trust_rank');
+    //   megaindexTrustRank = megaindexTrustRank ? megaindexTrustRank.innerText : '';
 
-      let megaindexDomainRank = document.querySelector('#set_domain_rank');
-      megaindexDomainRank = megaindexDomainRank ? megaindexDomainRank.innerText : '';
+    //   let megaindexDomainRank = document.querySelector('#set_domain_rank');
+    //   megaindexDomainRank = megaindexDomainRank ? megaindexDomainRank.innerText : '';
 
-      // let effectiveness = getRandomInRange(1, 100);
-      // let articles_num = getRandomInRange(10, 400);
-      let screen = domain+'.jpeg';
-      let chart = domain+'_chart.jpeg';
+    //   // let effectiveness = getRandomInRange(1, 100);
+    //   // let articles_num = getRandomInRange(10, 400);
+    //   let screen = domain+'.jpeg';
+    //   let chart = domain+'_chart.jpeg';
          
-      return {
-        "domain": domain,
-        "traffic": traffic, 
-        "organic": organic,
-        "direct": direct,
-        "traffic_season": traffic_season,
-        "project_stage": project_stage,
-        "profit_await": profit_await,
-        "evaluate_min": evaluate_min,
-        "evaluate_middle": evaluate_middle,
-        "evaluate_max": evaluate_max,
-        "domain_age": domain_age,
-        "IKS": IKS,
-        "index_Y": index_Y,
-        "index_G": index_G,
-        "megaindexTrustRank": megaindexTrustRank,
-        "megaindexDomainRank": megaindexDomainRank
-      }
-    }, (domain));
+    //   return {
+    //     "domain": domain,
+    //     "traffic": traffic, 
+    //     "organic": organic,
+    //     "direct": direct,
+    //     "traffic_season": traffic_season,
+    //     "project_stage": project_stage,
+    //     "profit_await": profit_await,
+    //     "evaluate_min": evaluate_min,
+    //     "evaluate_middle": evaluate_middle,
+    //     "evaluate_max": evaluate_max,
+    //     "domain_age": domain_age,
+    //     "IKS": IKS,
+    //     "index_Y": index_Y,
+    //     "index_G": index_G,
+    //     "megaindexTrustRank": megaindexTrustRank,
+    //     "megaindexDomainRank": megaindexDomainRank
+    //   }
+    // }, (domain));
     browser.close();
     console.log(JSON.stringify(be1));
     process.exit();
