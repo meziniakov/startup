9c2c4be57a982ae0a150d186075a6d3b29a6dd2a
mezin <z2941@ya.ru>
2021-10-30 14:33:35 +0300

    fix

---
+++ nodejs/test-be1.js
@@ -0,0 +1,43 @@
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const captchaAPI = require("./config.js");
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
+
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: captchaAPI
+    },
+    visualFeedback: true
+  })
+)
+
+let getBe1 = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+  await page.setViewport({
+    width: 1280,
+    height: 720,
+    deviceScaleFactor: 1,
+  });
+  let screen_path = '../storage/web/screen/';
+  let chart_path = '../storage/web/chart/';
+  let domain = process.argv[2];
+
+  try {
+    await page.goto('https://be1.ru/stat/'+domain);
+    await page.screenshot({path: screen_path+domain+'_'+new Date().getTime()+'.jpg', type: 'jpeg', quality: 80});
+    browser.close();
+    process.exit();
+  } catch(error) {
+    console.log(error);
+    process.exit();
+  }
+}
+
+getBe1().then((value) => {
+});
