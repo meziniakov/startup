20e0ef14126d3266dff213e91caa91fefd1d5866
mezin <z2941@ya.ru>
2021-11-29 00:34:51 +0300

    fix

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,9 +1,28 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const captchaAPI = require("./config.js");
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
+
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: captchaAPI // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+    },
+    visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
+  })
+)
 
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   const webmaster = `https://webmaster.yandex.ru/siteinfo/?host=otdyhsamostoyatelno.ru`;
+  await page.solveRecaptchas();
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(`.CheckboxCaptcha-Anchor input[type=submit]`)
+    // document.querySelector('.CheckboxCaptcha-Anchor input[type=submit]').click()
+  ])
   console.log(`Перехожу по адресу: ${webmaster}`);
   // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
   await page.goto(webmaster, {waitUntil: 'domcontentloaded'});
